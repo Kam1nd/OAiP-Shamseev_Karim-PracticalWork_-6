ba00b7e4736667dda3b967d7a9115bd26cd6d8ab
Kam1nd <shamseevk12@email.com>
2026-02-06 04:17:54 +0300

    Commit 1

---
+++ Practic_Shamseev_6/Practic_Shamseev_6/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        GamingLaptop myLaptop = new GamingLaptop
+        {
+            brand = "HP",
+            cpuModel = "Intel Core i5 - 12400",
+            ramCapacity = 32,
+            isPoweredOn = true,
+            gpuModel = "NVIDIA GeForce RTX 4070",
+            batteryLifeGaming = 90
+        };
+
+        Console.WriteLine("Calling methods via myLaptop object:");
+        myLaptop.BootSystem();
+        myLaptop.RunDiagnostics();
+        myLaptop.DisplayPerformanceMetrics();
+        myLaptop.OptimizeResources();
+        myLaptop.ConnectPeripherals();
+        myLaptop.ManageCoolingSystem();
+
+        Console.WriteLine();
+
+        Console.WriteLine("Calling methods via myComputer object:");
+
+        Computer myComputer = myLaptop;
+        myComputer.BootSystem();
+        myComputer.RunDiagnostics();
+        myComputer.DisplayPerformanceMetrics();
+        myComputer.OptimizeResources();
+        myComputer.ConnectPeripherals();
+        myComputer.ManageCoolingSystem();
+    }
+}
